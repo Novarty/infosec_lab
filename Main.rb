e4a3facc3eb0a13a4031ecb88c90bf9a9d7a68a8
Novarty <artem.ivanov9616@gmail.com>
2018-04-20 00:54:02 +0300

    push fix

--- Main.rb
+++
@@ -1,7 +0,0 @@
-require_relative "Twofish"
-
-key = ['5d9d4eeffa9151575524f115815a12e0']
-twofish = Twofish.new(key.pack("H*"))
-
-puts ciphertext = twofish.encrypt('Hello World!1!!; Lorem ipsum dolor sit amet')
-puts twofish.decrypt(ciphertext)
